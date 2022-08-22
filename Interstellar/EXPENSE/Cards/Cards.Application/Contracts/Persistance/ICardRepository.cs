8aa474ea40b1830345b7e0ac61a5ceb60653a311
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-08-22 19:02:21 +0530

    Persystence is Added

---
+++ Interstellar/EXPENSE/Cards/Cards.Application/Contracts/Persistance/ICardRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cards.Domain.Entity;
+using Cards.Domain.Enums;
+
+namespace Cards.Application.Contracts.Persistance
+{
+    public interface ICardRepository: IAsyncRepository<Card>
+    {
+        Task<IEnumerable<Card>> GetAllCards(CardTypes cardTypes);
+    }
+}
