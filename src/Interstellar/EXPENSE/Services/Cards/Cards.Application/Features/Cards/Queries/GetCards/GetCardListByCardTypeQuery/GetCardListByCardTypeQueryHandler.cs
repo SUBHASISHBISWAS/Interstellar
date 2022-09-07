51850038cf0c38ee5b7c487b9f393d345270d6ab
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:40:47 +0530

    Update GetCardListByCardTypeQueryHandler.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Queries/GetCards/GetCardListByCardTypeQuery/GetCardListByCardTypeQueryHandler.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Queries/GetCards/GetCardListByCardTypeQuery/GetCardListByCardTypeQueryHandler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using AutoMapper;
 
 using Cards.Application.Contracts.Persistance;
@@ -11,23 +6,22 @@ using Cards.Application.Features.Cards.Queries.GetCards.ViewModel;
 
 using MediatR;
 
-namespace Cards.Application.Features.Cards.Queries.GetCards.GetCardListByCardTypeQuery
+namespace Cards.Application.Features.Cards.Queries.GetCards.GetCardListByCardTypeQuery;
+
+public class GetCardListByCardTypeQueryHandler : IRequestHandler<GetCardListByCardTypeQuery, List<CardVm>>
 {
-    public class GetCardListByCardTypeQueryHandler : IRequestHandler<GetCardListByCardTypeQuery, List<CardVm>>
+    private readonly ICardRepository _cardRepository;
+    private readonly IMapper _mapper;
+
+    public GetCardListByCardTypeQueryHandler(ICardRepository cardRepository, IMapper mapper)
     {
-        private readonly ICardRepository _cardRepository;
-        private readonly IMapper _mapper;
+        _cardRepository = cardRepository ?? throw new ArgumentNullException(nameof(cardRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
 
-        public GetCardListByCardTypeQueryHandler(ICardRepository cardRepository, IMapper mapper)
-        {
-            _cardRepository = cardRepository ?? throw new ArgumentNullException(nameof(cardRepository));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-        }
-
-        public async Task<List<CardVm>> Handle(GetCardListByCardTypeQuery request, CancellationToken cancellationToken)
-        {
-            var cardList = await _cardRepository.GetAllCards(request.CardTypes);
-            return _mapper.Map<List<CardVm>>(cardList);
-        }
+    public async Task<List<CardVm>> Handle(GetCardListByCardTypeQuery request, CancellationToken cancellationToken)
+    {
+        var cardList = await _cardRepository.GetAllCards(request.CardTypes);
+        return _mapper.Map<List<CardVm>>(cardList);
     }
 }
