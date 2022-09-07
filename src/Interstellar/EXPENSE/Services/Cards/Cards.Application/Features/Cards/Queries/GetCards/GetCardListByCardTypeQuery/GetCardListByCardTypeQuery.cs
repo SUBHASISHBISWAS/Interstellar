a19f5d15621895e651f7cc20b282da947f1202e0
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:40:33 +0530

    Update GetCardListByCardTypeQuery.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Queries/GetCards/GetCardListByCardTypeQuery/GetCardListByCardTypeQuery.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Queries/GetCards/GetCardListByCardTypeQuery/GetCardListByCardTypeQuery.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Application.Features.Cards.Queries.GetCards.ViewModel;
 using Cards.Domain.Enums;
 
 using MediatR;
 
-namespace Cards.Application.Features.Cards.Queries.GetCards.GetCardListByCardTypeQuery
-{
-    public class GetCardListByCardTypeQuery : IRequest<List<CardVm>>
-    {
-        public CardTypes CardTypes { get; set; }
+namespace Cards.Application.Features.Cards.Queries.GetCards.GetCardListByCardTypeQuery;
 
-        public GetCardListByCardTypeQuery(CardTypes cardTypes)
-        {
-            CardTypes = cardTypes;
-        }
+public class GetCardListByCardTypeQuery : IRequest<List<CardVm>>
+{
+    public CardTypes CardTypes { get; set; }
+
+    public GetCardListByCardTypeQuery(CardTypes cardTypes)
+    {
+        CardTypes = cardTypes;
     }
 }
