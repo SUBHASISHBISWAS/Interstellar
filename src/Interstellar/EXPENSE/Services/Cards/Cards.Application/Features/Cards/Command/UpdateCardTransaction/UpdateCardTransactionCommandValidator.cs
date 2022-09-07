46c8290aac501dd0e071ba3750cc0bf472021d56
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:39:05 +0530

    Update UpdateCardTransactionCommandValidator.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/UpdateCardTransaction/UpdateCardTransactionCommandValidator.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/UpdateCardTransaction/UpdateCardTransactionCommandValidator.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
-using Cards.Application.Features.Cards.Command.UpdateCard;
-using FluentValidation;
+namespace Cards.Application.Features.Cards.Command.UpdateCardTransactions;
 
-namespace Cards.Application.Features.Cards.Command.UpdateCardTransactions
+public class UpdateCardTransactionCommandValidator : AbstractValidator<UpdateCardTransactionCommand>
 {
-    public class UpdateCardTransactionCommandValidator : AbstractValidator<UpdateCardTransactionCommand>
+    public UpdateCardTransactionCommandValidator()
     {
-        public UpdateCardTransactionCommandValidator()
-        {
-            RuleFor(p => p.CardId).NotEmpty().WithMessage("{CardId} is Required").NotNull().WithMessage("{CardId} should not be Null");
+        RuleFor(p => p.CardId).NotEmpty().WithMessage("{CardId} is Required").NotNull().WithMessage("{CardId} should not be Null");
 
-            RuleFor(p => p.CardSwipeAmount).NotEmpty().WithMessage("{CardTotalTotalExpenditure} is Required").GreaterThan(0).WithMessage("{CardTotalTotalExpenditure} should not be Empty");
-        }
+        RuleFor(p => p.CardSwipeAmount).NotEmpty().WithMessage("{CardTotalTotalExpenditure} is Required").GreaterThan(0).WithMessage("{CardTotalTotalExpenditure} should not be Empty");
     }
 }
