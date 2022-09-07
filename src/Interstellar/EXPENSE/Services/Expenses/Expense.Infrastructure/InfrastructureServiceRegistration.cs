013c610c30735ba81d9bc033b3ca36ec90b6e19e
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:29:15 +0530

    Update InfrastructureServiceRegistration.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,36 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Expense.Application.Contracts.Persistance;
-using Expense.Infrastructure.Persistence;
 using Expense.Infrastructure.Repository;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Expense.Infrastructure
+namespace Expense.Infrastructure;
+
+public static class InfrastructureServiceRegistration
 {
-    public static class InfrastructureServiceRegistration
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+
+        var assemblyName = typeof(ExpenseContext).Namespace;
+        services.AddDbContext<ExpenseContext>(options =>
         {
+            options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString"), options => options.MigrationsAssembly(assemblyName));
+        });
 
-            var assemblyName = typeof(ExpenseContext).Namespace;
-            services.AddDbContext<ExpenseContext>(options =>
-            {
-                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString"),options=>options.MigrationsAssembly(assemblyName));
-            });
+        services.AddScoped<ExpenseContext, ExpenseContext>();
+        services.AddScoped<IExpenseRepository, ExpenseRepository>();
 
-            services.AddScoped<ExpenseContext, ExpenseContext>();
-            services.AddScoped<IExpenseRepository, ExpenseRepository>();
-            
-            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
 
-            return services;
-        }
+        return services;
     }
 }
