435280f5b175a3df876dec4b67c1b1604b1fe228
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:53:34 +0530

    Update CreateExpenseCommandValidator.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Command/CreateExpense/CreateExpenseCommandValidator.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Command/CreateExpense/CreateExpenseCommandValidator.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using FluentValidation;
 
-namespace Expense.Application.Features.Expense.Command.CreateExpense
+namespace Expense.Application.Features.Expense.Command.CreateExpense;
+
+internal class CreateExpenseCommandValidator : AbstractValidator<CreateExpenseCommand>
 {
-    internal class CreateExpenseCommandValidator:AbstractValidator<CreateExpenseCommand>
+    public CreateExpenseCommandValidator()
     {
-        public CreateExpenseCommandValidator()
-        {
-            RuleFor(p=>p.ExpenseAmount).NotEmpty().WithMessage("{TransactionAmout} is Required");
+        RuleFor(p => p.ExpenseAmount).NotEmpty().WithMessage("{TransactionAmout} is Required");
 
-            RuleFor(p => p.ExpenseCardId).NotEmpty().WithMessage("{TransactionCard} is Required");
+        RuleFor(p => p.ExpenseCardId).NotEmpty().WithMessage("{TransactionCard} is Required");
 
-            RuleFor(p => p.ExpenseType).NotEmpty().WithMessage("{TransactionType} is Required");
-        }
+        RuleFor(p => p.ExpenseType).NotEmpty().WithMessage("{TransactionType} is Required");
     }
 }
