8bcf4c5bfc1f985b86ecd28c422b9cbb6fe9f0de
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:43:50 +0530

    Update CardContextSeed.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Infrastructure/Data/CardContextSeed.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Infrastructure/Data/CardContextSeed.cs
@@ -1,72 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Domain.Entity;
 
 using MongoDB.Driver;
 
-namespace Cards.Infrastructure.Data
+namespace Cards.Infrastructure.Data;
+
+public class CardContextSeed
 {
-    public class CardContextSeed
+    public static void SeedData(IMongoCollection<Card> cardCollection)
     {
-        public static void SeedData(IMongoCollection<Card> cardCollection)
+        bool isCardExist = cardCollection.Find(p => true).Any();
+        if (!isCardExist)
         {
-            bool isCardExist = cardCollection.Find(p => true).Any();
-            if (!isCardExist)
-            {
-                cardCollection.InsertManyAsync(GetPreconfiguredCard());
-            }
+            cardCollection.InsertManyAsync(GetPreconfiguredCard());
         }
+    }
 
-        private static IEnumerable<Card> GetPreconfiguredCard()
+    private static IEnumerable<Card> GetPreconfiguredCard()
+    {
+        return new List<Card>()
         {
-            return new List<Card>()
+            new Card()
             {
-                new Card()
-                {
-                    CardName="HDFC GLOBAL CREDIT CARD",
-                    CardDescription="HDFC GLOBAL CREDIT CARD",
-                    CardNumber="1234-5678-1234-5678",
-                    CardType=Domain.Enums.CardTypes.VISA,
-                    CardExpieryDate=DateTime.ParseExact("31/12/2027", "dd/MM/yyyy", null).Date,
-                    CardStatementDate=DateTime.ParseExact("18/09/2022", "dd/MM/yyyy", null).Date,
-                    CardTotalExpenditure=1,
-                    CardCurrentMonthExpenditure=0,
-                    CardNextMonthExpenditure=0,
-                    CardNextStatementDate=DateTime.ParseExact("18/09/2022", "dd/MM/yyyy", null).Date.AddMonths(1),
-                    CardDueDate=DateTime.ParseExact("18/09/2022", "dd/MM/yyyy", null).Date.AddDays(50).AddMonths(-1),
-                    CardTransactions=new List<string>(),
-                    GracePeriod=50,
-                    CreatedDate=DateTime.Now.Date,
-                    LastModifiedBy="SUBHASISH",
-                    LastModifiedDate=DateTime.Now,
-                    CreatedBy="SUBHASISH"
+                CardName="HDFC GLOBAL CREDIT CARD",
+                CardDescription="HDFC GLOBAL CREDIT CARD",
+                CardNumber="1234-5678-1234-5678",
+                CardType=Domain.Enums.CardTypes.VISA,
+                CardExpieryDate=DateTime.ParseExact("31/12/2027", "dd/MM/yyyy", null).Date,
+                CardStatementDate=DateTime.ParseExact("18/09/2022", "dd/MM/yyyy", null).Date,
+                CardTotalExpenditure=1,
+                CardCurrentMonthExpenditure=0,
+                CardNextMonthExpenditure=0,
+                CardNextStatementDate=DateTime.ParseExact("18/09/2022", "dd/MM/yyyy", null).Date.AddMonths(1),
+                CardDueDate=DateTime.ParseExact("18/09/2022", "dd/MM/yyyy", null).Date.AddDays(50).AddMonths(-1),
+                CardTransactions=new List<string>(),
+                GracePeriod=50,
+                CreatedDate=DateTime.Now.Date,
+                LastModifiedBy="SUBHASISH",
+                LastModifiedDate=DateTime.Now,
+                CreatedBy="SUBHASISH"
 
-                },
-                new Card()
-                {
-                    CardName="ICIC CORAL CREDIT CARD",
-                    CardDescription="ICICI CORAL CREDIT CARD",
-                    CardNumber="1234-5678-1234-5678",
-                    CardType=Domain.Enums.CardTypes.VISA,
-                    CardExpieryDate=DateTime.ParseExact("31/12/2027", "dd/MM/yyyy", null).Date,
-                    CardStatementDate=DateTime.ParseExact("18/09/2022", "dd/MM/yyyy", null).Date,
-                    CardTotalExpenditure=1,
-                    CardCurrentMonthExpenditure=0,
-                    CardNextMonthExpenditure=0,
-                    CardNextStatementDate=DateTime.ParseExact("18/09/2023", "dd/MM/yyyy", null).Date.AddMonths(1),
-                    CardDueDate=DateTime.ParseExact("18/09/2022", "dd/MM/yyyy", null).Date.AddDays(49).AddMonths(-1),
-                    CardTransactions=new List<string>(),
-                    GracePeriod=49,
-                    CreatedDate=DateTime.Now.Date,
-                    LastModifiedBy="SUBHASISH",
-                    LastModifiedDate=DateTime.Now,
-                    CreatedBy="SUBHASISH",
-                },
-            };
-        }
+            },
+            new Card()
+            {
+                CardName="ICIC CORAL CREDIT CARD",
+                CardDescription="ICICI CORAL CREDIT CARD",
+                CardNumber="1234-5678-1234-5678",
+                CardType=Domain.Enums.CardTypes.VISA,
+                CardExpieryDate=DateTime.ParseExact("31/12/2027", "dd/MM/yyyy", null).Date,
+                CardStatementDate=DateTime.ParseExact("18/09/2022", "dd/MM/yyyy", null).Date,
+                CardTotalExpenditure=1,
+                CardCurrentMonthExpenditure=0,
+                CardNextMonthExpenditure=0,
+                CardNextStatementDate=DateTime.ParseExact("18/09/2023", "dd/MM/yyyy", null).Date.AddMonths(1),
+                CardDueDate=DateTime.ParseExact("18/09/2022", "dd/MM/yyyy", null).Date.AddDays(49).AddMonths(-1),
+                CardTransactions=new List<string>(),
+                GracePeriod=49,
+                CreatedDate=DateTime.Now.Date,
+                LastModifiedBy="SUBHASISH",
+                LastModifiedDate=DateTime.Now,
+                CreatedBy="SUBHASISH",
+            },
+        };
     }
 }
