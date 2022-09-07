8ea875f0cf7c030f356e2d53b10212a4c262920f
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:29:55 +0530

    Update CardTransactionUpdateConsumer.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.API/EventBusConsumer/CardTransactionUpdateConsumer.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.API/EventBusConsumer/CardTransactionUpdateConsumer.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 
-using Cards.Application.Features.Cards.Command.UpdateCard;
 using Cards.Application.Features.Cards.Command.UpdateCardTransactions;
 
 using EventBus.Messages.Events;
@@ -9,28 +8,27 @@ using MassTransit;
 
 using MediatR;
 
-namespace Cards.API.EventBusConsumer
+namespace Cards.API.EventBusConsumer;
+
+public class CardTransactionUpdateConsumer : IConsumer<CardSwipeEvent>
 {
-    public class CardTransactionUpdateConsumer:IConsumer<CardSwipeEvent>
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+    private readonly ILogger<CardTransactionUpdateConsumer> _logger;
+
+    public CardTransactionUpdateConsumer(IMediator mediator, IMapper mapper, ILogger<CardTransactionUpdateConsumer> logger)
     {
-        private readonly IMediator _mediator;
-        private readonly IMapper _mapper;
-        private readonly ILogger<CardTransactionUpdateConsumer> _logger;
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
 
-        public CardTransactionUpdateConsumer(IMediator mediator, IMapper mapper, ILogger<CardTransactionUpdateConsumer> logger)
-        {
-            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        }
+    public async Task Consume(ConsumeContext<CardSwipeEvent> context)
+    {
+        var command = _mapper.Map<UpdateCardTransactionCommand
+            >(context.Message);
+        var result = await _mediator.Send(command);
+        _logger.LogInformation("CardUpdate Event consumed successfully. Updated Card Id : {newOrderId}", result);
 
-        public async Task Consume(ConsumeContext<CardSwipeEvent> context)
-        {
-            var command = _mapper.Map<UpdateCardTransactionCommand
-                >(context.Message);
-            var result = await _mediator.Send(command);
-            _logger.LogInformation("CardUpdate Event consumed successfully. Updated Card Id : {newOrderId}", result);
-            
-        }
     }
 }
