8fc69078c4aa18a75f1ceb758d678056a236a8e3
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:34:33 +0530

    Update CreateCardCommand.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/CreateCard/CreateCardCommand.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/CreateCard/CreateCardCommand.cs
@@ -1,30 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Cards.Domain.Enums;
-
+﻿
 using MediatR;
 
-namespace Cards.Application.Features.Cards.Command.CreateCard
+namespace Cards.Application.Features.Cards.Command.CreateCard;
+
+public class CreateCardCommand : IRequest<string>
 {
-    public class CreateCardCommand : IRequest<string>
-    {
 
-        public string? CardName { get; set; }
+    public string? CardName { get; set; }
 
-        public string? CardType { get; set; }
+    public string? CardType { get; set; }
 
-        public string? CardNumber { get; set; }
+    public string? CardNumber { get; set; }
 
-        public string? CardDescription { get; set; }
+    public string? CardDescription { get; set; }
 
-        public DateTime CardExpieryDate { get; set; }
+    public DateTime CardExpieryDate { get; set; }
 
-        public DateTime CardStatementDate { get; set; }
+    public DateTime CardStatementDate { get; set; }
 
-        public int GracePeriod { get; set; }
-    }
+    public int GracePeriod { get; set; }
 }
