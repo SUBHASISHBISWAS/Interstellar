5a2ce1d5eb2ce46725a6fc0f47130ee14f9f387e
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:36:32 +0530

    Update UpdateCardCommand.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/UpdateCard/UpdateCardCommand.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/UpdateCard/UpdateCardCommand.cs
@@ -1,36 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Domain.Enums;
 
 using MediatR;
 
-using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
-namespace Cards.Application.Features.Cards.Command.UpdateCard
+namespace Cards.Application.Features.Cards.Command.UpdateCard;
+
+public class UpdateCardCommand : IRequest<string>
 {
-    public class UpdateCardCommand:IRequest<string>
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? CardId { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? CardId { get; set; }
 
-        public string? CardName { get; set; }
+    public string? CardName { get; set; }
 
-        public CardTypes CardType { get; set; }
+    public CardTypes CardType { get; set; }
 
-        public string? CardNumber { get; set; }
+    public string? CardNumber { get; set; }
 
-        public string? CardDescription { get; set; }
+    public string? CardDescription { get; set; }
 
-        public DateTime CardExpieryDate { get; set; }
+    public DateTime CardExpieryDate { get; set; }
 
-        public DateTime CardStatementDate { get; set; }
+    public DateTime CardStatementDate { get; set; }
 
-        public double CardTotalExpenditure { get; set; }
-    }
+    public double CardTotalExpenditure { get; set; }
 }
