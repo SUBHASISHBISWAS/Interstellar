a3e58467cf9b774a0071b5df55da0ae7ec7d4520
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:33:36 +0530

    Update NotFoundException.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Exceptions/NotFoundException.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Exceptions/NotFoundException.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Cards.Application.Exceptions;
 
-namespace Cards.Application.Exceptions
+public class NotFoundException : ApplicationException
 {
-    public class NotFoundException : ApplicationException
+    public NotFoundException(string name, object key)
+        : base($"Entity \"{name}\" ({key}) was not found.")
     {
-        public NotFoundException(string name, object key)
-            : base($"Entity \"{name}\" ({key}) was not found.")
-        {
-        }
     }
 }
