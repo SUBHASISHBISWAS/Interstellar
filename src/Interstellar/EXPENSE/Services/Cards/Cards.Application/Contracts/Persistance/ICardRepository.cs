ea003c3d1052d84a360ded0c45dca36c45973368
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:33:09 +0530

    Update ICardRepository.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Contracts/Persistance/ICardRepository.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Contracts/Persistance/ICardRepository.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Domain.Entity;
 using Cards.Domain.Enums;
 
-namespace Cards.Application.Contracts.Persistance
+namespace Cards.Application.Contracts.Persistance;
+
+public interface ICardRepository : IAsyncRepository<Card>
 {
-    public interface ICardRepository: IAsyncRepository<Card>
-    {
-        Task<IEnumerable<Card>> GetAllCards(CardTypes cardTypes);
-    }
+    Task<IEnumerable<Card>> GetAllCards(CardTypes cardTypes);
 }
