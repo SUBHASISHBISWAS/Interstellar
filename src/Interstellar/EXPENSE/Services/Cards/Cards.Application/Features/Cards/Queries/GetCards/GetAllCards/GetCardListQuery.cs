08f1aaebce5c1848f8f2397f77aa76fce51dbfc5
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:39:35 +0530

    Update GetCardListQuery.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Queries/GetCards/GetAllCards/GetCardListQuery.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Queries/GetCards/GetAllCards/GetCardListQuery.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Application.Features.Cards.Queries.GetCards.ViewModel;
 
 using MediatR;
 
-namespace Cards.Application.Features.Cards.Queries.GetCards.GetAllCards
+namespace Cards.Application.Features.Cards.Queries.GetCards.GetAllCards;
+
+public class GetCardListQuery : IRequest<List<CardVm>>
 {
-    public class GetCardListQuery : IRequest<List<CardVm>>
-    {
-    }
 }
