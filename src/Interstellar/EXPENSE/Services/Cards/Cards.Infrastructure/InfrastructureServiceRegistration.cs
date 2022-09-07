98200591f849ec077b986ff11aed8f3627e4ebb4
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:44:52 +0530

    Update InfrastructureServiceRegistration.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Application.Contracts.Persistance;
-using Cards.Domain.Entity;
 using Cards.Infrastructure.Data;
 using Cards.Infrastructure.Repository;
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Cards.Infrastructure
+namespace Cards.Infrastructure;
+
+public static class InfrastructureServiceRegistration
 {
-    public static class InfrastructureServiceRegistration
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddScoped<ICardRepository, CardRepository>();
-            services.AddScoped<ICardContext, CardContext>();
-            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
-            
-            return services;
-        }
+        services.AddScoped<ICardRepository, CardRepository>();
+        services.AddScoped<ICardContext, CardContext>();
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+
+        return services;
     }
 }
