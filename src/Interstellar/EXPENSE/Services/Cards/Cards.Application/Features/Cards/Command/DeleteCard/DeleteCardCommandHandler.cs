16c9cf41755b64b91974d989d66ee8c1d966f2de
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:36:08 +0530

    Update DeleteCardCommandHandler.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/DeleteCard/DeleteCardCommandHandler.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/DeleteCard/DeleteCardCommandHandler.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using AutoMapper;
+
 using Cards.Application.Contracts.Persistance;
 using Cards.Application.Exceptions;
 using Cards.Application.Features.Cards.Command.CreateCard;
@@ -14,36 +10,35 @@ using MediatR;
 
 using Microsoft.Extensions.Logging;
 
-namespace Cards.Application.Features.Cards.Command.DeleteCard
+namespace Cards.Application.Features.Cards.Command.DeleteCard;
+
+public class DeleteCardCommandHandler : IRequestHandler<DeleteCardCommand>
 {
-    public class DeleteCardCommandHandler : IRequestHandler<DeleteCardCommand>
+    private readonly ICardRepository _cardRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger<CreateCardCommandHandler> _logger;
+
+    public DeleteCardCommandHandler(ICardRepository cardRepository, IMapper mapper, ILogger<CreateCardCommandHandler> logger)
     {
-        private readonly ICardRepository _cardRepository;
-        private readonly IMapper _mapper;
-        private readonly ILogger<CreateCardCommandHandler> _logger;
+        _cardRepository = cardRepository ?? throw new ArgumentNullException(nameof(cardRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
 
-        public DeleteCardCommandHandler(ICardRepository cardRepository, IMapper mapper, ILogger<CreateCardCommandHandler> logger)
+    public async Task<Unit> Handle(DeleteCardCommand request, CancellationToken cancellationToken)
+    {
+        var cardToDelete = await _cardRepository.GetByIdAsync(request.CardId!);
+
+        if (cardToDelete == null)
         {
-            _cardRepository = cardRepository ?? throw new ArgumentNullException(nameof(cardRepository));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _logger.LogError("Card does not Exist in database");
+            throw new NotFoundException(nameof(Card), request.CardId!);
         }
 
-        public async Task<Unit> Handle(DeleteCardCommand request, CancellationToken cancellationToken)
-        {
-            var cardToDelete = await _cardRepository.GetByIdAsync(request.CardId!);
+        await _cardRepository.DeleteAsync(cardToDelete);
 
-            if (cardToDelete == null)
-            {
-                _logger.LogError("Card does not Exist in database");
-                throw new NotFoundException(nameof(Card), request.CardId!);
-            }
+        _logger.LogInformation($"Card {cardToDelete.CardId} is successfully deleted.");
 
-            await _cardRepository.DeleteAsync(cardToDelete);
-
-            _logger.LogInformation($"Card {cardToDelete.CardId} is successfully deleted.");
-
-            return Unit.Value;
-        }
+        return Unit.Value;
     }
 }
