e30fbd231491f182d59fb5647090f76e191976d9
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:20:13 +0530

    Update GetExpensesQueryHandler.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Queries/GetExpenses/GetExpensesQueryHandler.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Queries/GetExpenses/GetExpensesQueryHandler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using AutoMapper;
 
 using Expense.Application.Contracts.Persistance;
@@ -11,24 +6,23 @@ using Expense.Application.Features.Expense.ViewModels;
 
 using MediatR;
 
-namespace Expense.Application.Features.Expense.Queries.GetExpenses
+namespace Expense.Application.Features.Expense.Queries.GetExpenses;
+
+public class GetExpensesQueryHandler : IRequestHandler<GetExpensesQuery, List<ExpenseVm>>
 {
-    public class GetExpensesQueryHandler : IRequestHandler<GetExpensesQuery, List<ExpenseVm>>
+
+    private readonly IExpenseRepository _expenseRepository;
+    private readonly IMapper _mapper;
+
+    public GetExpensesQueryHandler(IExpenseRepository expenseRepository, IMapper mapper)
     {
+        _expenseRepository = expenseRepository ?? throw new ArgumentNullException(nameof(expenseRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
 
-        private readonly IExpenseRepository _expenseRepository;
-        private readonly IMapper _mapper;
-
-        public GetExpensesQueryHandler(IExpenseRepository expenseRepository, IMapper mapper)
-        {
-            _expenseRepository = expenseRepository ?? throw new ArgumentNullException(nameof(expenseRepository));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-        }
-
-        public async Task<List<ExpenseVm>> Handle(GetExpensesQuery request, CancellationToken cancellationToken)
-        {
-            var expenseList = await _expenseRepository.GetAllAsync();
-            return _mapper.Map<List<ExpenseVm>>(expenseList);
-        }
+    public async Task<List<ExpenseVm>> Handle(GetExpensesQuery request, CancellationToken cancellationToken)
+    {
+        var expenseList = await _expenseRepository.GetAllAsync();
+        return _mapper.Map<List<ExpenseVm>>(expenseList);
     }
 }
