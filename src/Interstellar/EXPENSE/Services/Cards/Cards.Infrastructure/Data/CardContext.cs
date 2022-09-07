347b86f7b5dfe8725f8ebc863a6c00b7d0917c73
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:43:42 +0530

    Update CardContext.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Infrastructure/Data/CardContext.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Infrastructure/Data/CardContext.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Domain.Entity;
 
 using Microsoft.Extensions.Configuration;
 
 using MongoDB.Driver;
 
-namespace Cards.Infrastructure.Data
-{
-    public class CardContext : ICardContext
-    {
-        public CardContext(IConfiguration configuration)
-        {
-            var x = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
-            var client = new MongoClient(x);
-            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
-            Cards = database.GetCollection<Card>(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+namespace Cards.Infrastructure.Data;
 
-            CardContextSeed.SeedData(Cards);
-        }
-        public IMongoCollection<Card> Cards { get; }
+public class CardContext : ICardContext
+{
+    public CardContext(IConfiguration configuration)
+    {
+        var x = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+        var client = new MongoClient(x);
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+        Cards = database.GetCollection<Card>(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+        CardContextSeed.SeedData(Cards);
     }
+    public IMongoCollection<Card> Cards { get; }
 }
