18f78023dcfcd7215fe4667ba7df3f0e6ccddb42
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:27:23 +0530

    Update ExpenseRepository.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Infrastructure/Repository/ExpenseRepository.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Infrastructure/Repository/ExpenseRepository.cs
@@ -1,25 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Expense.Application.Contracts.Persistance;
 using Expense.Domain.Entities;
-using Expense.Infrastructure.Persistence;
 
-namespace Expense.Infrastructure.Repository
+namespace Expense.Infrastructure.Repository;
+
+public class ExpenseRepository : RepositoryBase<ExpenseEntity>, IExpenseRepository
 {
-    public class ExpenseRepository : RepositoryBase<ExpenseEntity>, IExpenseRepository
+
+    public ExpenseRepository(ExpenseContext context) : base(context)
     {
 
-        public ExpenseRepository(ExpenseContext context) : base(context)
-        {
-
-        }
-        public Task<IEnumerable<ExpenseEntity>> GetExpenseByExpenseType(string userName)
-        {
-            throw new NotImplementedException();
-        }
+    }
+    public Task<IEnumerable<ExpenseEntity>> GetExpenseByExpenseType(string userName)
+    {
+        throw new NotImplementedException();
     }
 }
