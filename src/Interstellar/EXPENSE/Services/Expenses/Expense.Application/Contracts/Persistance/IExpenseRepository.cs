d78d5ab2f641ce7739b42f49ae350cb1b7d3a63e
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:51:32 +0530

    Update IExpenseRepository.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Contracts/Persistance/IExpenseRepository.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Contracts/Persistance/IExpenseRepository.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Expense.Domain.Entities;
 
-namespace Expense.Application.Contracts.Persistance
+namespace Expense.Application.Contracts.Persistance;
+
+public interface IExpenseRepository : IAsyncRepository<ExpenseEntity>
 {
-    public interface IExpenseRepository : IAsyncRepository<ExpenseEntity>
-    {
-        Task<IEnumerable<ExpenseEntity>> GetExpenseByExpenseType(string userName);
-    }
+    Task<IEnumerable<ExpenseEntity>> GetExpenseByExpenseType(string userName);
 }
