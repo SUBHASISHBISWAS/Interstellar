2c1ccbb01c5fc50f2b80049d836fb2ab36878e42
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:24:54 +0530

    Update ExpenseEntity.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Domain/Entities/ExpenseEntity.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Domain/Entities/ExpenseEntity.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Expense.Domain.Common;
 
-namespace Expense.Domain.Entities
+namespace Expense.Domain.Entities;
+
+public class ExpenseEntity : EntityBase
 {
-    public class ExpenseEntity : EntityBase
-    {
-        public double ExpenseAmount { get; set; }
+    public double ExpenseAmount { get; set; }
 
-        public string? ExpenseType { get; set; }
+    public string? ExpenseType { get; set; }
 
-        public string? ExpenseDecription { get; set; }
+    public string? ExpenseDecription { get; set; }
 
-        public string? ExpenseCardId { get; set; }
+    public string? ExpenseCardId { get; set; }
 
-        public DateTime ExpenseDate { get; set; }
+    public DateTime ExpenseDate { get; set; }
 
-    }
 }
