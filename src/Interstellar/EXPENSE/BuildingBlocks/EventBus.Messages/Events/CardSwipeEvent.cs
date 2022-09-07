48e14ee9136c01825000439a9b31e0d7da3b3a5d
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:48:54 +0530

    Update CardSwipeEvent.cs

--- src/Interstellar/EXPENSE/BuildingBlocks/EventBus.Messages/Events/CardSwipeEvent.cs
+++ src/Interstellar/EXPENSE/BuildingBlocks/EventBus.Messages/Events/CardSwipeEvent.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace EventBus.Messages.Events;
 
-namespace EventBus.Messages.Events
+public class CardSwipeEvent : IntegrationBaseEvent
 {
-    public class CardSwipeEvent:IntegrationBaseEvent
-    {
-        public string? CardId { get; set; }
+    public string? CardId { get; set; }
 
-        public double CardSwipeAmount { get; set; }
+    public double CardSwipeAmount { get; set; }
 
-        public int ExpenseId { get; set; }
+    public int ExpenseId { get; set; }
 
-        public DateTime ExpenseDate { get; set; }
-    }
+    public DateTime ExpenseDate { get; set; }
 }
