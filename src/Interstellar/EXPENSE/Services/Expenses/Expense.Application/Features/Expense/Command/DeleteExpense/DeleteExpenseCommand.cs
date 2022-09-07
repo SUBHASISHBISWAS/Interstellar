1bce4471ef9303cf307a65c30677eab1e6ce0a36
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:18:22 +0530

    Update DeleteExpenseCommand.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Command/DeleteExpense/DeleteExpenseCommand.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Command/DeleteExpense/DeleteExpenseCommand.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using MediatR;
 
-namespace Expense.Application.Features.Expense.Command.DeleteExpense
+namespace Expense.Application.Features.Expense.Command.DeleteExpense;
+
+public class DeleteExpenseCommand : IRequest
 {
-    public class DeleteExpenseCommand:IRequest
-    {
-        public int TransactionId { get; set; }
-    }
+    public int TransactionId { get; set; }
 }
