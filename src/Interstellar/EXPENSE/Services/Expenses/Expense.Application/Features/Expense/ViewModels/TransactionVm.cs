fb540967c4828c35e560990763546b80e173a847
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:20:31 +0530

    Update TransactionVm.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/ViewModels/TransactionVm.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/ViewModels/TransactionVm.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Expense.Application.Features.Expense.ViewModels;
 
-namespace Expense.Application.Features.Expense.ViewModels
+public class ExpenseVm
 {
-    public class ExpenseVm
-    {
-        public int ExpenseId { get; set; }
-        public double ExpenseAmount { get; set; }
+    public int ExpenseId { get; set; }
+    public double ExpenseAmount { get; set; }
 
-        public string? ExpenseType { get; set; }
+    public string? ExpenseType { get; set; }
 
-        public string? ExpenseDecription { get; set; }
+    public string? ExpenseDecription { get; set; }
 
-        public string? ExpenseCardId { get; set; }
+    public string? ExpenseCardId { get; set; }
 
-        public DateTime ExpenseDate { get; set; }
-    }
+    public DateTime ExpenseDate { get; set; }
 }
