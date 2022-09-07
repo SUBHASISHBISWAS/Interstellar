b7830464263b84a64cb79acf7032e55e43cd7206
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:37:58 +0530

    Update UpdateCardCommandValidator.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/UpdateCard/UpdateCardCommandValidator.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/UpdateCard/UpdateCardCommandValidator.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Application.Features.Cards.Command.UpdateCard;
 
 using FluentValidation;
 
-namespace Cards.Application.Features.Cards.Command.CreateCard
-{
-    public class UpdateCardCommandValidator : AbstractValidator<UpdateCardCommand>
-    {
-        public UpdateCardCommandValidator()
-        {
-            RuleFor(p => p.CardName).NotEmpty().WithMessage("{CardName} is Required").NotNull().MaximumLength(20).WithMessage("{CardName} must not exceed 20 Character");
-        }
+namespace Cards.Application.Features.Cards.Command.CreateCard;
 
+public class UpdateCardCommandValidator : AbstractValidator<UpdateCardCommand>
+{
+    public UpdateCardCommandValidator()
+    {
+        RuleFor(p => p.CardName).NotEmpty().WithMessage("{CardName} is Required").NotNull().MaximumLength(20).WithMessage("{CardName} must not exceed 20 Character");
     }
+
 }
