7965ce789e3ba646b7cbc375ceb0d79ba86f041c
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:53:10 +0530

    Update CreateExpenseCommand.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Command/CreateExpense/CreateExpenseCommand.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Command/CreateExpense/CreateExpenseCommand.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Expense.Domain.Entities;
 
 using MediatR;
 
-namespace Expense.Application.Features.Expense.Command.CreateExpense
+namespace Expense.Application.Features.Expense.Command.CreateExpense;
+
+public class CreateExpenseCommand : IRequest<ExpenseEntity>
 {
-    public class CreateExpenseCommand:IRequest<ExpenseEntity>
-    {
-       
-        public double ExpenseAmount { get; set; }
 
-        public string? ExpenseType { get; set; }
+    public double ExpenseAmount { get; set; }
 
-        public string? ExpenseDecription { get; set; }
+    public string? ExpenseType { get; set; }
 
-        public string? ExpenseCardId { get; set; }
+    public string? ExpenseDecription { get; set; }
 
-        public DateTime ExpenseDate { get; set; }
-    }
+    public string? ExpenseCardId { get; set; }
+
+    public DateTime ExpenseDate { get; set; }
 }
