9a03713aa2bfd710cecaba161815bd86046df588
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:44:05 +0530

    Update ICardContext.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Infrastructure/Data/ICardContext.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Infrastructure/Data/ICardContext.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Domain.Entity;
 
 using MongoDB.Driver;
 
-namespace Cards.Infrastructure.Data
+namespace Cards.Infrastructure.Data;
+
+public interface ICardContext
 {
-    public interface ICardContext
-    {
-        IMongoCollection<Card> Cards { get; }
-    }
+    IMongoCollection<Card> Cards { get; }
 }
