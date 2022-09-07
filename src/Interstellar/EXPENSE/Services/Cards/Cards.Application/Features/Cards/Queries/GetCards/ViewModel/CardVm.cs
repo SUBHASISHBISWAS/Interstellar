17403b0f937f3b83bba2cc7a1940f89212c3e7cc
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:41:04 +0530

    Update CardVm.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Queries/GetCards/ViewModel/CardVm.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Queries/GetCards/ViewModel/CardVm.cs
@@ -1,38 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Domain.Enums;
 
-namespace Cards.Application.Features.Cards.Queries.GetCards.ViewModel
+namespace Cards.Application.Features.Cards.Queries.GetCards.ViewModel;
+
+public class CardVm
 {
-    public class CardVm
-    {
-        public string? CardId { get; set; }
+    public string? CardId { get; set; }
 
-        public string? CardName { get; set; }
+    public string? CardName { get; set; }
 
-        public CardTypes CardType { get; set; }
+    public CardTypes CardType { get; set; }
 
-        public string? CardNumber { get; set; }
+    public string? CardNumber { get; set; }
 
-        public string? CardDescription { get; set; }
+    public string? CardDescription { get; set; }
 
-        public DateTime CardExpieryDate { get; set; }
+    public DateTime CardExpieryDate { get; set; }
 
-        public DateTime CardStatementDate { get; set; }
+    public DateTime CardStatementDate { get; set; }
 
-        public DateTime CardNextStatementDate { get; set; }
-        public DateTime CardDueDate { get; set; }
-        
-        public int GracePeriod { get; set; }
+    public DateTime CardNextStatementDate { get; set; }
+    public DateTime CardDueDate { get; set; }
 
-        public double CardCurrentMonthExpenditure { get; set; }
+    public int GracePeriod { get; set; }
 
-        public double CardNextMonthExpenditure { get; set; }
+    public double CardCurrentMonthExpenditure { get; set; }
 
-        public double CardTotalExpenditure { get; set; }
-    }
+    public double CardNextMonthExpenditure { get; set; }
+
+    public double CardTotalExpenditure { get; set; }
 }
