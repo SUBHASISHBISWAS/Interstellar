b1dc32c84290de20d632b87d1c3fdf2464e5c108
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:37:26 +0530

    Update UpdateCardCommandHandler.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/UpdateCard/UpdateCardCommandHandler.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Command/UpdateCard/UpdateCardCommandHandler.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using AutoMapper;
+
 using Cards.Application.Contracts.Persistance;
 using Cards.Application.Exceptions;
 using Cards.Application.Features.Cards.Command.CreateCard;
@@ -14,38 +10,37 @@ using MediatR;
 
 using Microsoft.Extensions.Logging;
 
-namespace Cards.Application.Features.Cards.Command.UpdateCard
+namespace Cards.Application.Features.Cards.Command.UpdateCard;
+
+internal class UpdateCardCommandHandler : IRequestHandler<UpdateCardCommand, string>
 {
-    internal class UpdateCardCommandHandler : IRequestHandler<UpdateCardCommand,string>
+    private readonly ICardRepository _cardRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger<CreateCardCommandHandler> _logger;
+
+    public UpdateCardCommandHandler(ICardRepository cardRepository, IMapper mapper, ILogger<CreateCardCommandHandler> logger)
     {
-        private readonly ICardRepository _cardRepository;
-        private readonly IMapper _mapper;
-        private readonly ILogger<CreateCardCommandHandler> _logger;
+        _cardRepository = cardRepository ?? throw new ArgumentNullException(nameof(cardRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
 
-        public UpdateCardCommandHandler(ICardRepository cardRepository, IMapper mapper, ILogger<CreateCardCommandHandler> logger)
+    public async Task<string> Handle(UpdateCardCommand request, CancellationToken cancellationToken)
+    {
+        var cardToUpdate = await _cardRepository.GetByIdAsync(request.CardId!);
+
+        if (cardToUpdate == null)
         {
-            _cardRepository = cardRepository ?? throw new ArgumentNullException(nameof(cardRepository));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _logger.LogError("Card does not Exist in database");
+            throw new NotFoundException(nameof(Card), request.CardId!);
         }
 
-        public async Task<string> Handle(UpdateCardCommand request, CancellationToken cancellationToken)
-        {
-            var cardToUpdate = await _cardRepository.GetByIdAsync(request.CardId!);
+        _mapper.Map(request, cardToUpdate, typeof(UpdateCardCommand), typeof(Card));
 
-            if (cardToUpdate==null)
-            {
-                _logger.LogError("Card does not Exist in database");
-                throw new NotFoundException(nameof(Card), request.CardId!);
-            }
+        await _cardRepository.UpdateAsync(cardToUpdate);
 
-            _mapper.Map(request, cardToUpdate, typeof(UpdateCardCommand), typeof(Card));
+        _logger.LogInformation($"Order {cardToUpdate} is successfully updated.");
 
-            await _cardRepository.UpdateAsync(cardToUpdate);
-
-            _logger.LogInformation($"Order {cardToUpdate} is successfully updated.");
-
-            return cardToUpdate.CardId!;
-        }
+        return cardToUpdate.CardId!;
     }
 }
