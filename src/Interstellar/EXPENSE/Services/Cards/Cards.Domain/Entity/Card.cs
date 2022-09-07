7dc3d9f83b3a8742fd7b4f3498dbec4c05b179e9
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:42:49 +0530

    Update Card.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Domain/Entity/Card.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Domain/Entity/Card.cs
@@ -1,49 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Domain.Common;
 using Cards.Domain.Enums;
 
-namespace Cards.Domain.Entity
+namespace Cards.Domain.Entity;
+
+public class Card : EntityBase
 {
-    public class Card:EntityBase
-    {
-        
-        public string? CardName { get; set; }
-       
-        public CardTypes CardType { get; set; }
 
-        public string? CardNumber { get; set; }
+    public string? CardName { get; set; }
 
-        public string? CardDescription { get; set; }
+    public CardTypes CardType { get; set; }
 
-        public DateTime CardExpieryDate { get; set; }
+    public string? CardNumber { get; set; }
 
-        public DateTime CardStatementDate { get; set; }
+    public string? CardDescription { get; set; }
 
-        public DateTime CardDueDate { get; set; }
+    public DateTime CardExpieryDate { get; set; }
+
+    public DateTime CardStatementDate { get; set; }
+
+    public DateTime CardDueDate { get; set; }
 
 
-        public DateTime CardNextStatementDate { get; set; }
+    public DateTime CardNextStatementDate { get; set; }
 
-        public int GracePeriod { get; set; }
-
-        
-        public double CardCurrentMonthExpenditure { get; set; }
-
-        
-        public double CardNextMonthExpenditure { get; set; }
-        
-        
-        public double CardTotalExpenditure { get; set; }
+    public int GracePeriod { get; set; }
 
 
-        public List<string>? CardTransactions { get; set; }
+    public double CardCurrentMonthExpenditure { get; set; }
 
-        
-    }
+
+    public double CardNextMonthExpenditure { get; set; }
+
+
+    public double CardTotalExpenditure { get; set; }
+
+
+    public List<string>? CardTransactions { get; set; }
+
+
 }
