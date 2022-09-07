221c1e304fd88e55629d5847e5481b3a4e9121cd
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:26:33 +0530

    Update ExpenseContext.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Infrastructure/Persistence/ExpenseContext.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Infrastructure/Persistence/ExpenseContext.cs
@@ -1,44 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Expense.Domain.Common;
 using Expense.Domain.Entities;
 
 using Microsoft.EntityFrameworkCore;
 
-namespace Expense.Infrastructure
+namespace Expense.Infrastructure;
+
+public class ExpenseContext : DbContext
 {
-    public class ExpenseContext : DbContext
+    public DbSet<ExpenseEntity>? Transactions { get; set; }
+
+    public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
     {
-        public DbSet<ExpenseEntity>? Transactions { get; set; }
 
-        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<EntityBase>())
         {
-
-        }
-
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-        {
-            foreach (var entry  in ChangeTracker.Entries<EntityBase>())
+            switch (entry.State)
             {
-                switch (entry.State)
-                {
-                    case EntityState.Added:
-                        entry.Entity.CreatedDate = DateTime.Now;
-                        entry.Entity.CreatedBy = "subhasish";
-                        break;
-                    case EntityState.Modified:
-                        entry.Entity.LastModifiedDate = DateTime.Now;
-                        entry.Entity.LastModifiedBy = "subhasish";
-                        break;
-                    default:
-                        break;
-                }
+                case EntityState.Added:
+                    entry.Entity.CreatedDate = DateTime.Now;
+                    entry.Entity.CreatedBy = "subhasish";
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedDate = DateTime.Now;
+                    entry.Entity.LastModifiedBy = "subhasish";
+                    break;
+                default:
+                    break;
             }
-            return base.SaveChangesAsync(cancellationToken);
         }
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
