d1ab7ce875d18c2b7fcab8b4b5eb0a54ae8f12ac
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:19:59 +0530

    Update GetExpensesQuery.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Queries/GetExpenses/GetExpensesQuery.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Queries/GetExpenses/GetExpensesQuery.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Expense.Application.Features.Expense.ViewModels;
 
 using MediatR;
 
-namespace Expense.Application.Features.Expense.Queries.GetExpenses
+namespace Expense.Application.Features.Expense.Queries.GetExpenses;
+
+public class GetExpensesQuery : IRequest<List<ExpenseVm>>
 {
-    public class GetExpensesQuery : IRequest<List<ExpenseVm>>
-    {
-    }
 }
