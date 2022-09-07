bc2072e31a033a5cfe9249dcbcd5bc2c3c781be6
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:33:55 +0530

    Update ValidationException.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Exceptions/ValidationException.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Exceptions/ValidationException.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using FluentValidation.Results;
 
-namespace Cards.Application.Exceptions
-{
-    public class ValidationException : ApplicationException
-    {
-        public IDictionary<string, string[]> Errors { get; }
-        public ValidationException() : base("One or More Validation Failure Happens")
-        {
-            Errors = new Dictionary<string, string[]>();
-        }
+namespace Cards.Application.Exceptions;
 
-        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
-        {
-            Errors = failures.
-                GroupBy(e => e.PropertyName, e => e.ErrorMessage).
-                ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
-        }
+public class ValidationException : ApplicationException
+{
+    public IDictionary<string, string[]> Errors { get; }
+    public ValidationException() : base("One or More Validation Failure Happens")
+    {
+        Errors = new Dictionary<string, string[]>();
+    }
+
+    public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+    {
+        Errors = failures.
+            GroupBy(e => e.PropertyName, e => e.ErrorMessage).
+            ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
     }
 }
