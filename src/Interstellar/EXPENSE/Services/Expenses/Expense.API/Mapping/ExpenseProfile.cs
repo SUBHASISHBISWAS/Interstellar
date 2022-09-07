75278c6979a02b7cb268eb0dcc0cb3057a4fb68e
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:50:10 +0530

    Update ExpenseProfile.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.API/Mapping/ExpenseProfile.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.API/Mapping/ExpenseProfile.cs
@@ -4,13 +4,12 @@ using EventBus.Messages.Events;
 
 using Expense.Domain.Entities;
 
-namespace Expense.API.Mapping
+namespace Expense.API.Mapping;
+
+public class ExpenseProfile : Profile
 {
-    public class ExpenseProfile:Profile
+    public ExpenseProfile()
     {
-        public ExpenseProfile()
-        {
-            CreateMap<ExpenseEntity, CardSwipeEvent>().ForMember(dest => dest.CardId, opt => opt.MapFrom(src => src.ExpenseCardId)).ForMember(dest => dest.CardSwipeAmount, opt => opt.MapFrom(src => src.ExpenseAmount)).ForMember(dest=>dest.ExpenseId,opt=>opt.MapFrom(src=>src.ExpenseId)).ForMember(dest => dest.ExpenseDate, opt => opt.MapFrom(src => src.ExpenseDate));
-        }
+        CreateMap<ExpenseEntity, CardSwipeEvent>().ForMember(dest => dest.CardId, opt => opt.MapFrom(src => src.ExpenseCardId)).ForMember(dest => dest.CardSwipeAmount, opt => opt.MapFrom(src => src.ExpenseAmount)).ForMember(dest => dest.ExpenseId, opt => opt.MapFrom(src => src.ExpenseId)).ForMember(dest => dest.ExpenseDate, opt => opt.MapFrom(src => src.ExpenseDate));
     }
 }
