4f5af7aeb5f7da93eba7e668f27ed9de792d2792
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:26:50 +0530

    Update ExpenseContextSeed.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Infrastructure/Persistence/ExpenseContextSeed.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Infrastructure/Persistence/ExpenseContextSeed.cs
@@ -1,42 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Expense.Domain.Entities;
 
 using Microsoft.Extensions.Logging;
 
-namespace Expense.Infrastructure.Persistence
+namespace Expense.Infrastructure.Persistence;
+
+public class ExpenseContextSeed
 {
-    public class ExpenseContextSeed
+
+    public static async Task SeedAsync(ExpenseContext expenseContext, ILogger<ExpenseContextSeed> logger)
     {
-
-        public static async Task SeedAsync(ExpenseContext expenseContext, ILogger<ExpenseContextSeed> logger)
+        if (!expenseContext.Transactions.Any())
         {
-            if (!expenseContext.Transactions.Any())
+            expenseContext.Transactions.AddRange(GetPreconfiguredOrders());
+            await expenseContext.SaveChangesAsync();
+            logger.LogInformation("Seed database associated with context {DbContextName}", typeof(ExpenseContextSeed).Name);
+        }
+    }
+
+    private static IEnumerable<ExpenseEntity> GetPreconfiguredOrders()
+    {
+        return new List<ExpenseEntity>
+        {
+            new ExpenseEntity()
             {
-                expenseContext.Transactions.AddRange(GetPreconfiguredOrders());
-                await expenseContext.SaveChangesAsync();
-                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(ExpenseContextSeed).Name);
+                ExpenseAmount=100,
+                ExpenseDate=DateTime.Now,
+                ExpenseCardId="6304d7cb142a8d1bc9a41916",
+                ExpenseDecription="Bought Apple Phone",
+                ExpenseType="Card",
+
             }
-        }
-
-        private static IEnumerable<ExpenseEntity> GetPreconfiguredOrders()
-        {
-            return new List<ExpenseEntity>
-            {
-                new ExpenseEntity()
-                {
-                    ExpenseAmount=100,
-                    ExpenseDate=DateTime.Now,
-                    ExpenseCardId="6304d7cb142a8d1bc9a41916",
-                    ExpenseDecription="Bought Apple Phone",
-                    ExpenseType="Card",
-                    
-                }
-            };
-        }
+        };
     }
 }
