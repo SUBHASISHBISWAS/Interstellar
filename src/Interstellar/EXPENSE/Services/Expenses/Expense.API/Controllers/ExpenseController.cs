e04c3d8250058d556025fe74b5623ecc51c3605c
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:49:37 +0530

    Update ExpenseController.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.API/Controllers/ExpenseController.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.API/Controllers/ExpenseController.cs
@@ -16,62 +16,60 @@ using MediatR;
 
 using Microsoft.AspNetCore.Mvc;
 
-namespace Expense.API.Controllers
+namespace Expense.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ExpenseController : ControllerBase
 {
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
+    private readonly IPublishEndpoint _publishEndpoint;
 
-    [ApiController]
-    [Route("api/v1/[controller]")]
-    public class ExpenseController:ControllerBase
+    public ExpenseController(IMediator mediator, IPublishEndpoint publishEndpoint, IMapper mapper)
     {
-        private readonly IMapper _mapper;
-        private readonly IMediator _mediator;
-        private readonly IPublishEndpoint _publishEndpoint;
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
 
-        public ExpenseController(IMediator mediator, IPublishEndpoint publishEndpoint, IMapper mapper)
-        {
-            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-        }
+    [HttpGet(Name = "GetExpenses")]
+    [ProducesResponseType(typeof(IEnumerable<ExpenseVm>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<IEnumerable<ExpenseVm>>> GetAllExpenses()
+    {
+        var query = new GetExpensesQuery();
+        var expenses = await _mediator.Send(query);
+        return Ok(expenses);
+    }
 
-        [HttpGet(Name = "GetExpenses")]
-        [ProducesResponseType(typeof(IEnumerable<ExpenseVm>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<IEnumerable<ExpenseVm>>> GetAllExpenses()
-        {
-            var query = new GetExpensesQuery();
-            var expenses = await _mediator.Send(query);
-            return Ok(expenses);
-        }
+    [HttpPost(Name = "CreateExpense")]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    public async Task<ActionResult<int>> CreateExpense([FromBody] CreateExpenseCommand command)
+    {
+        var result = await _mediator.Send(command);
+        var cardToUpdateMessage = _mapper.Map<CardSwipeEvent>(result);
+        await _publishEndpoint.Publish(cardToUpdateMessage);
+        return Ok(result);
+    }
 
-        [HttpPost(Name = "CreateExpense")]
-        [ProducesResponseType((int)HttpStatusCode.OK)]
-        public async Task<ActionResult<int>> CreateExpense([FromBody] CreateExpenseCommand command)
-        {
-            var result = await _mediator.Send(command);
-            var cardToUpdateMessage=_mapper.Map<CardSwipeEvent>(result);
-            await _publishEndpoint.Publish(cardToUpdateMessage);
-            return Ok(result);
-        }
+    [HttpPut(Name = "UpdateExpense")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> UpdateExpense([FromBody] UpdateExpenseCommand command)
+    {
+        await _mediator.Send(command);
+        return NoContent();
+    }
 
-        [HttpPut(Name = "UpdateExpense")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesDefaultResponseType]
-        public async Task<ActionResult> UpdateExpense([FromBody] UpdateExpenseCommand command)
-        {
-            await _mediator.Send(command);
-            return NoContent();
-        }
-
-        [HttpDelete("{transactionId}", Name = "DeleteExpense")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesDefaultResponseType]
-        public async Task<ActionResult> DeleteExpense(int transactionId)
-        {
-            var command = new DeleteExpenseCommand() { TransactionId = transactionId };
-            await _mediator.Send(command);
-            return NoContent();
-        }
+    [HttpDelete("{transactionId}", Name = "DeleteExpense")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> DeleteExpense(int transactionId)
+    {
+        var command = new DeleteExpenseCommand() { TransactionId = transactionId };
+        await _mediator.Send(command);
+        return NoContent();
     }
 }
