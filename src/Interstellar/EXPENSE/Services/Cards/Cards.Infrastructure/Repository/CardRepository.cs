ad42cd4f1bca1c81979739b980783e2e6c732ce3
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:44:24 +0530

    Update CardRepository.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Infrastructure/Repository/CardRepository.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Infrastructure/Repository/CardRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
+﻿
 using Cards.Application.Contracts.Persistance;
 using Cards.Domain.Entity;
 using Cards.Domain.Enums;
@@ -12,18 +6,17 @@ using Cards.Infrastructure.Data;
 
 using MongoDB.Driver;
 
-namespace Cards.Infrastructure.Repository
-{
-    internal class CardRepository : RepositoryBase<Card>, ICardRepository
-    {
-        public CardRepository(ICardContext context) : base(context)
-        {
-        }
+namespace Cards.Infrastructure.Repository;
 
-        public async Task<IEnumerable<Card>> GetAllCards(CardTypes cardTypes)
+internal class CardRepository : RepositoryBase<Card>, ICardRepository
 {
-            FilterDefinition<Card> filter = Builders<Card>.Filter.Eq(p => p.CardType, cardTypes);
-            return await _context.Cards.Find(filter).ToListAsync();
-        }
+    public CardRepository(ICardContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Card>> GetAllCards(CardTypes cardTypes)
+    {
+        FilterDefinition<Card> filter = Builders<Card>.Filter.Eq(p => p.CardType, cardTypes);
+        return await _context.Cards.Find(filter).ToListAsync();
     }
 }
