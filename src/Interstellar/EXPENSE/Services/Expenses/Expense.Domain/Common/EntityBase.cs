d8050f4213536d451be5b6a2c9186b0ab257bee6
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:23:02 +0530

    Update EntityBase.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Domain/Common/EntityBase.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Domain/Common/EntityBase.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Expense.Domain.Common
+namespace Expense.Domain.Common;
+
+public abstract class EntityBase
 {
-    public abstract class EntityBase
-    {
-        [Key]
-        public int ExpenseId { get; set; }
-        public string? CreatedBy { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public string? LastModifiedBy { get; set; }
-        public DateTime? LastModifiedDate { get; set; }
-    }
+    [Key]
+    public int ExpenseId { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string? LastModifiedBy { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
 }
