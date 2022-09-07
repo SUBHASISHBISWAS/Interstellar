2ce6d4e42c0e39dca615246f6c4eda86946db0d5
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:40:02 +0530

    Update GetCardByIdQuery.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Queries/GetCards/GetCardById/GetCardByIdQuery.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Application/Features/Cards/Queries/GetCards/GetCardById/GetCardByIdQuery.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Cards.Application.Features.Cards.Queries.GetCards.ViewModel;
 
 using MediatR;
 
-namespace Cards.Application.Features.Cards.Queries.GetCards.GetCardById
-{
-    public class GetCardByIdQuery:IRequest<CardVm>
-    {
-        public string Id { get; set; }
+namespace Cards.Application.Features.Cards.Queries.GetCards.GetCardById;
 
-        public GetCardByIdQuery(string id)
-        {
-            Id = id ?? throw new ArgumentNullException(nameof(id));
-        }
+public class GetCardByIdQuery : IRequest<CardVm>
+{
+    public string Id { get; set; }
+
+    public GetCardByIdQuery(string id)
+    {
+        Id = id ?? throw new ArgumentNullException(nameof(id));
     }
 }
