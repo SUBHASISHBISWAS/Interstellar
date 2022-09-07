4a89e5568fb3d81c555d330dfb0c645626c0bcea
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 10:42:17 +0530

    Update EntityBase.cs

--- src/Interstellar/EXPENSE/Services/Cards/Cards.Domain/Common/EntityBase.cs
+++ src/Interstellar/EXPENSE/Services/Cards/Cards.Domain/Common/EntityBase.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace Cards.Domain.Common
+namespace Cards.Domain.Common;
+
+public abstract class EntityBase
 {
-    public abstract class EntityBase
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? CardId { get; protected set; }
-        public string? CreatedBy { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public string? LastModifiedBy { get; set; }
-        public DateTime? LastModifiedDate { get; set; }
-    }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? CardId { get; protected set; }
+    public string? CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string? LastModifiedBy { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
 }
