2a7c8d157febe56820cc9e513ef5e9d4a3d34385
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-07 11:18:40 +0530

    Update DeleteExpenseCommandHandler.cs

--- src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Command/DeleteExpense/DeleteExpenseCommandHandler.cs
+++ src/Interstellar/EXPENSE/Services/Expenses/Expense.Application/Features/Expense/Command/DeleteExpense/DeleteExpenseCommandHandler.cs
@@ -1,51 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using AutoMapper;
+
 using Expense.Application.Contracts.Persistance;
 using Expense.Application.Exceptions;
 using Expense.Application.Features.Expense.Command.CreateExpense;
-using Expense.Application.Features.Expense.Command.UpdateExpense;
 using Expense.Domain.Entities;
 
 using MediatR;
 
 using Microsoft.Extensions.Logging;
 
-namespace Expense.Application.Features.Expense.Command.DeleteExpense
+namespace Expense.Application.Features.Expense.Command.DeleteExpense;
+
+public class DeleteExpenseCommandHandler : IRequestHandler<DeleteExpenseCommand>
 {
-    public class DeleteExpenseCommandHandler : IRequestHandler<DeleteExpenseCommand>
+
+    private readonly IExpenseRepository _expenseRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger<CreateExpenseCommandHandler> _logger;
+
+    public DeleteExpenseCommandHandler(IExpenseRepository expenseRepository, IMapper mapper, ILogger<CreateExpenseCommandHandler> logger)
     {
+        _expenseRepository = expenseRepository ?? throw new ArgumentNullException(nameof(expenseRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
 
-        private readonly IExpenseRepository _expenseRepository;
-        private readonly IMapper _mapper;
-        private readonly ILogger<CreateExpenseCommandHandler> _logger;
+    public async Task<Unit> Handle(DeleteExpenseCommand request, CancellationToken cancellationToken)
+    {
+        var expenseToDelete = await _expenseRepository.GetByIdAsync(request.TransactionId);
 
-        public DeleteExpenseCommandHandler(IExpenseRepository expenseRepository, IMapper mapper, ILogger<CreateExpenseCommandHandler> logger)
+        if (expenseToDelete == null)
         {
-            _expenseRepository = expenseRepository ?? throw new ArgumentNullException(nameof(expenseRepository));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _logger.LogError("Transation does not Exist in Database");
+            throw new NotFoundException(nameof(ExpenseEntity), request.TransactionId);
         }
 
-        public async Task<Unit> Handle(DeleteExpenseCommand request, CancellationToken cancellationToken)
-        {
-            var expenseToDelete = await _expenseRepository.GetByIdAsync(request.TransactionId);
+        await _expenseRepository.DeleteAsync(expenseToDelete);
 
-            if (expenseToDelete == null)
-            {
-                _logger.LogError("Transation does not Exist in Database");
-                throw new NotFoundException(nameof(ExpenseEntity), request.TransactionId);
-            }
+        _logger.LogInformation($"Expense  {expenseToDelete.ExpenseId} is Successfully Deleted");
 
-            await _expenseRepository.DeleteAsync(expenseToDelete);
-
-            _logger.LogInformation($"Expense  {expenseToDelete.ExpenseId} is Successfully Deleted");
-
-            return  Unit.Value;
-        }
+        return Unit.Value;
     }
 }
