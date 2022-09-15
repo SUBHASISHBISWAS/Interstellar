505b4b3031ffe70b651545d7d797eb4bdf8e003c
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-09-15 22:05:04 +0530

    Update card.module.ts

--- src/Interstellar/EXPENSE/GUI/WEBGUI/Expense.WEBGUI/src/app/card/card.module.ts
+++ src/Interstellar/EXPENSE/GUI/WEBGUI/Expense.WEBGUI/src/app/card/card.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DisplayCardsComponent } from './display-cards/display-cards.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CardData } from './Data/CardData';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 @NgModule({
   declarations: [DisplayCardsComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InMemoryWebApiModule.forRoot(CardData),
+  ],
 })
 export class CardModule {}
