932562b2dc721b45f9358c281cb52c3dc736a56d
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-12-28 10:24:40 +0530

    Update card.module.ts

--- src/Interstellar/EXPENSE/GUI/WEBGUI/Expense.WEBGUI/src/app/card/card.module.ts
+++ src/Interstellar/EXPENSE/GUI/WEBGUI/Expense.WEBGUI/src/app/card/card.module.ts
@@ -1,21 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DisplayCardsComponent } from './display-cards/display-cards.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CardData } from './Data/CardData';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { CreateCardComponent } from './create-card/create-card.component';
 import { TextMaskModule } from 'angular2-text-mask';
 import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { CreateCardComponent } from './create-card/create-card.component';
+import { CardData } from './Data/CardData';
 import { CreditCardDirective } from './Directives/CreditCardNumber';
 import { OnlyNumberDirective } from './Directives/OnlyNuberForCardNumber';
+import { DisplayCardsComponent } from './display-cards/display-cards.component';
 
 @NgModule({
   declarations: [
