d4d311ab81396f7688cb92bad56a32ad8cb6b9d5
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-12-28 10:25:11 +0530

    Update app.module.ts

--- src/Interstellar/EXPENSE/GUI/WEBGUI/Expense.WEBGUI/src/app/app.module.ts
+++ src/Interstellar/EXPENSE/GUI/WEBGUI/Expense.WEBGUI/src/app/app.module.ts
@@ -1,26 +1,13 @@
-import { HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { AppRoutingModule } from './Routing/app-routing.module';
-
 import { AppComponent } from './app.component';
 import { CardModule } from './card/card.module';
 import { LoginModule } from './Login/login.module';
-import { RegisterUserComponent } from './Login/register-user/register-user.component';
-import { DatePipe } from '@angular/common';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    SharedModule,
-    CardModule,
-    LoginModule,
-    
-  ],
+  imports: [SharedModule, CardModule, LoginModule],
   providers: [DatePipe],
   bootstrap: [AppComponent],
 })
