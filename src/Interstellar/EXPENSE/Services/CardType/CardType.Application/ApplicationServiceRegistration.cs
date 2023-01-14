82b255b7a95113a62ae2997d62a77014379d37e4
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-01-14 17:20:50 +0530

    ApplicationServiceRegistration is Added

---
+++ src/Interstellar/EXPENSE/Services/CardType/CardType.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using CardType.Application.Behaviours;
+
+using FluentValidation;
+
+using MediatR;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CardType.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            
+            
+            return services;
+        }
+    }
+}
