ab7ac281d42da2031c26796777d73c174fabab3f
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-01-14 17:38:39 +0530

    Update Program.cs

--- src/Interstellar/EXPENSE/Services/CardType/CardType.API/Program.cs
+++ src/Interstellar/EXPENSE/Services/CardType/CardType.API/Program.cs
@@ -1,3 +1,5 @@
+using CardType.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Custom Service Registration
+builder.Services.AddApplicationServices();
+
+builder.Services.AddAutoMapper(typeof(Program));
+
 
 var app = builder.Build();
 
